d48116ee0b7f94ef6d3dd4ac468abe3b879b165e
Erlangga Dwi Pratama <adikaraerlangga@gmail.com>
2020-03-12 10:59:36 +0700

    Add files via upload

---
+++ .circleci/workflow.ps1
@@ -0,0 +1,24 @@
+﻿Workflow restart-resume
+{
+	npm --add-python-to-path install --global --production windows-build-tools --vs2015
+
+	Restart-Computer -Wait
+
+	yarn
+      	yarn add electron-builder --dev
+      	yarn run dist --win
+
+	Unregister-ScheduleJob -Name SetupResume
+}
+
+$adm = "erlanggaadikara"
+$pwd = ConvertTo-SecureString - String "Capoeirabboy1297" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($adm, $pwd)
+$AtStartup = New-JobTrigger -Name SetupResume `
+			    -Credential $ `
+			    -Trigger $AtStartup `
+			    -ScriptBlock {Import-Module PSWorkflow; `
+			    	Get-Job -Name srvSetup -State Suspend `
+				| Resume-Job}
+
+restart-resume -JobName srvSetup
